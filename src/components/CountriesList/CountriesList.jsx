aa7b38953060c66c17fb95a0a374d9803628192c
artweise <art2wsxweise7ujm@gmail.com>
2023-03-02 16:23:15 +0200

    iteration 2.2 done

---
+++ src/components/CountriesList/CountriesList.jsx
@@ -0,0 +1,5 @@
+const CountriesList = () => {
+  return <div></div>;
+};
+
+export default CountriesList;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from 'react-router-dom';
+import './style.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="navbar-inner-ctn">
+        <NavLink to="/" className="navbar-brand fs-2 fw-lighter">
+          LAB - WikiCountries
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
